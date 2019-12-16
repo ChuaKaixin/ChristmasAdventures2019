febe8754e67e2b55de0b941ca2148afd4110d1d1
Sarah Chua <43328484+ChuaKaixin@users.noreply.github.com>
2019-12-16 14:57:18 +0800

    Create Panel.java
    Panel for robot painting

---
+++ src/main/java/algo/dao/Panel.java
@@ -0,0 +1,15 @@
+package algo.dao;
+
+public class Panel {
+	private int color;
+	public int getColor() {
+		return color;
+	}
+	public void setColor(int color) {
+		this.color = color;
+	}
+	
+	public Panel(int color) {
+		this.color = color;
+	}
+}
