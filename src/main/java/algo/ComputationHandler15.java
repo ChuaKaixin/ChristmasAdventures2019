4d2541e10d0e564ae1317a1ce971ebee1e690e27
Sarah Chua <43328484+ChuaKaixin@users.noreply.github.com>
2019-12-20 15:02:20 +0800

    second draft: draw out the maze

--- src/main/java/algo/ComputationHandler15.java
+++ src/main/java/algo/ComputationHandler15.java
@@ -8,18 +8,19 @@ import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Stack;
 
 import algo.dao.Amplifier2;
 
 public class ComputationHandler15 {
 
 	public static void main(String[] args) throws Exception{
-		InputStream in = ComputationHandler.class.getClass().getResourceAsStream("/Day9Input.txt");
+		InputStream in = ComputationHandler15.class.getClass().getResourceAsStream("/Day15Input.txt");
 		List<BigInteger> inputsOriginal = readFromInputStream(in);
 
 		Amplifier2[]amplifiers = prepareAmplifiers(inputsOriginal, new int[] {1});
 		runAmplifier(amplifiers[0]);
-		System.out.println("FINAL value is :" + amplifiers[0].getLastOutput().toString());
+		System.out.println("PROGRAM ENDED");
 	}
 	
 	public static Amplifier2[] prepareAmplifiers(List<BigInteger>inputsOriginal, int[]permutation) {
@@ -54,6 +55,17 @@ public class ComputationHandler15 {
 		BigInteger output = BigInteger.ZERO;
 		BigInteger exitCode = new BigInteger("99");
 		int relativeBase = 0;
+		
+		int[][] map = new int[50][50];
+		
+		int currentX = 25;
+		int currentY = 25;
+		int nextMove = 1;
+		boolean backtrack = false;
+		int[]moveResult = new int[3];
+		Stack<Integer>moveStack = new Stack<>();
+		moveStack.push(nextMove);
+		
 		while(!input.equals(exitCode)) {
 		    System.out.println("current Index:" + currentIndex + " input: " + input);
 		    if(currentIndex == 353) {
@@ -83,16 +95,32 @@ public class ComputationHandler15 {
 			case 3:
 				value = determineValue(inputs, instructions[2], currentIndex+1, relativeBase);
 				System.out.println("Op [3] Set index" + value +
-						" to value : " + new BigInteger("2"));
-				setValueToInputs(inputs, currentIndex+1, new BigInteger("2"), relativeBase, instructions[2]);
+						" to value : " + BigInteger.valueOf(nextMove));
+				setValueToInputs(inputs, currentIndex+1, BigInteger.valueOf(nextMove), relativeBase, instructions[2]);
 				indexToMove = 2;
 				break;
 			case 4:
 				output = determineValue(inputs, instructions[2], currentIndex+1,relativeBase);
-				amp.addOutput(output);
+				moveResult = plotRobot(map, currentX, currentY, nextMove, output.intValue(), moveStack, backtrack);
 				System.out.println("OUTPUT: " + 
 						output
 						);
+				if(output.intValue()==2) {
+					System.out.println("REACHED GOAL!!! STOP");
+					input = exitCode;
+					printMap(map);
+					continue;
+				} else if(moveResult[2]==0) {
+					System.out.println("ERROR!! No valid move");
+					printMap(map);
+					input = exitCode;
+					continue;
+				} else {
+					currentX = moveResult[0];
+					currentY = moveResult[1];
+					nextMove = moveResult[2];
+					backtrack = moveResult[3]==1;
+				}
 				indexToMove = 2;
 				break;
 			case 5:
@@ -150,6 +178,20 @@ public class ComputationHandler15 {
 		amp.setExited();
 	}
 	
+	private static void printMap(int[][] map) {
+		for(int y=0; y<map.length; y++) {
+			for(int x=0; x<map[0].length; x++) {
+				String toprint = null;
+				if(x==25 && y==25) toprint = "!";
+				else if (map[y][x]==9) toprint = "#";
+				else if(map[y][x]==8 || map[y][x]==0) toprint = ".";
+				else toprint = "*";
+				System.out.print(toprint);
+			}
+			System.out.println("-");
+		}
+	}
+
 	private static BigInteger getValueFromInputs(List<BigInteger> inputs, int index) {
 		if(index >= inputs.size()) {
 			return BigInteger.ZERO;
@@ -180,8 +222,13 @@ public class ComputationHandler15 {
 		return getValueFromInputs(inputs,index);
 	}
 	
-	private static int[] plotRobot(int[][]map, int currentX, int currentY, int previousMove, int moveResult) {
-		int[] robotInfo = new int[3];
+	private static int[] plotRobot(int[][]map, int currentX, int currentY, int previousMove, int moveResult, Stack<Integer>moveStack, boolean isBacktrack) {
+		if(currentX == 12 && currentY==41 && previousMove==2 && moveResult==0) {
+			System.out.println("CHECKPOINT--");
+		}
+		if(currentX == 11 && currentY==40 && previousMove==3 && moveResult==0) {
+			System.out.println("CHECKPOINT--");
+		}
 		/**
 		 * 0:new X
 		 * 1:new Y
@@ -192,32 +239,129 @@ public class ComputationHandler15 {
 		 * 8: covered before
 		 * 1: goal
 		 */
-		int newX = currentX;
-		int newY = currentY;
-		if(moveResult == 0) {
-			switch(previousMove) {
-				case 1://north
-					markWall(map, currentY+1, currentX);
-					break;
-				case 2://south
-					markWall(map, currentY-1, currentX);
-					break;
-				case 3://east
-					markWall(map, currentY, currentX+1);
-					break;
-				case 4://west
-					markWall(map, currentY, currentX-1);
-					break;
+		int[]newLocation = recordMove(map, currentX, currentY, previousMove, moveResult, moveStack, isBacktrack);
+		int decidedMove = 0;
+		int backtrack = 0;
+		for (int move =1; move <=4; move++) {
+			if(checkMove(map, newLocation[0], newLocation[1], move, 0)) {
+				decidedMove = move;
+				break;
 			}
-		} else if(moveResult == 1) {
-			
 		}
-		
-		return robotInfo;
+		//move to some location you already visited if it has unreached nodes 
+		if(decidedMove ==0) {
+			//check for already visited nodes
+			for (int move =1; move <=4; move++) {
+				if(checkMove(map, newLocation[0], newLocation[1], move, 1)) {
+					decidedMove = move;
+					break;
+				}
+			}
+		}
+		//back track if no other choice
+		if(decidedMove == 0) {
+			if(!moveStack.empty()) {
+				int previousDoneMove = moveStack.pop();
+				switch(previousDoneMove) {
+					case 1:
+						decidedMove = 2;
+						break;
+					case 2:
+						decidedMove = 1;
+						break;
+					case 3:
+						decidedMove = 4;
+						break;
+					case 4: 
+						decidedMove = 3;
+						break;
+				}
+				backtrack = 1;
+			}
+		}
+		System.out.println("currentX:" + currentX + " currentY:" + currentY + " previousMove:" + previousMove + " moveResult:" + moveResult);
+
+		System.out.println("newX:" + newLocation[0] + " newY:" + newLocation[1] + " decided:" + decidedMove);
+		return new int[] {newLocation[0], newLocation[1], decidedMove, backtrack};
 	}
 	
-	private static void markWall(int[][]map, int x, int y) {
-		map[y][x] = 9;
+	private static boolean checkMove(int[][]map, int x, int y, int moveTry, int moveType) {
+		/**
+		 * moveType
+		 * 0: uncovered area
+		 * 1: covered area
+		 */
+		boolean goodMove = true;
+		int attemptedX = x;
+		int attemptedY = y;
+		switch(moveTry) {
+		case 1://north
+			attemptedY+=1;
+			break;
+		case 2://south
+			attemptedY-=1;
+			break;
+		case 4://east
+			attemptedX+=1;
+			break;
+		case 3://west
+			attemptedX-=1;
+			break;
+		}
+		if(moveType == 0) {
+			if(map[attemptedY][attemptedX] == 0) {return goodMove;}
+			else 
+				return false;
+		}
+		else if(map[attemptedY][attemptedX] == 8) {
+			if(
+				map[attemptedY+1][attemptedX] ==0 ||
+				map[attemptedY-1][attemptedX] ==0 ||
+				map[attemptedY][attemptedX+1] ==0 ||
+				map[attemptedY][attemptedX-1] ==0 
+					)
+				return goodMove;
+		}
+		return false;
+	}
+	
+	private static int[] recordMove(int[][]map, int x, int y, int previousMove, int moveResult, Stack<Integer>moveStack, boolean isBackTrack) {
+		int[] newLocation = new int[] {x,y};
+		int coveredX = x;
+		int coveredY = y;
+		switch(previousMove) {
+			case 1://north
+				coveredY+=1;
+				break;
+			case 2://south
+				coveredY-=1;
+				break;
+			case 4://east
+				coveredX+=1;
+				break;
+			case 3://west
+				coveredX-=1;
+				break;
+		}
+		int locationMarker = 0;
+		switch(moveResult) {
+			case 0:
+				locationMarker = 9;
+				break;
+			case 1:
+				locationMarker = 8;
+				newLocation = new int[] {coveredX, coveredY};
+				if(!isBackTrack)
+					moveStack.push(previousMove);
+				break;
+			case 2:
+				locationMarker = 1;
+				newLocation = new int[] {coveredX, coveredY};
+				break;
+		}
+		map[coveredY][coveredX] = locationMarker;
+		
+		return newLocation;
 	}
 	
 	private static int[] interpretCode(int code) {
